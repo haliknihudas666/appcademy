d6b60124a5b4023f503214240dd29dac8d3a8bae
rn-santos227 <rn_santos227@yahoo.com>
2019-11-09 22:18:16 +0800

    added rest request

--- lib/utils/API.dart
+++ lib/utils/API.dart
@@ -1,11 +1,21 @@
 import 'dart:async';
 
+import 'package:appcademy_hackaton/models/trivia.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 const baseUrl = "http://numbersapi.com/random/trivia?json";
 
 class API {
-  static Future getTrivia() {
-    return http.get(baseUrl);
+  static Future<Post> getTrivia() async {
+    final response = await http.get(baseUrl);
+
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON.
+      return Post.fromJson(json.decode(response.body));
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
   }
 }
